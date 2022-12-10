75b237c6c09cb0334017814b1bd06ee404e04988
Boris Moreno <boris_marco_moreno@hotmail.com>
2022-12-10 03:44:33 -0500

    enable ssl

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'strapi-database.cfvfjkkyjya9.us-east-1.rds.amazonaws.com'),
+      host: env('DATABASE_HOST', 'dpg-cea0i994reb52upbe17g-a.oregon-postgres.render.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'Postgres810'),
-      ssl: env.bool('DATABASE_SSL', false),
+      database: env('DATABASE_NAME', 'strapi_qt26'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'F8gqIMeH05fhSW08fJ5jPWjM6ECiL20t'),
+      ssl: env.bool('DATABASE_SSL', true),
     },
     useNullAsDefault: true,
   },
