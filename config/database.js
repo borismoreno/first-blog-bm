f7c2865fd426b7cb0808cdc588a8a2b092cdb957
Boris Moreno <boris_marco_moreno@hotmail.com>
2022-12-10 08:59:51 -0500

    add certificate

--- config/database.js
+++ config/database.js
@@ -1,13 +1,16 @@
+const fs = require('fs');
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'dpg-cea0i994reb52upbe17g-a.oregon-postgres.render.com'),
+      host: env('DATABASE_HOST', 'strapi-database.cfvfjkkyjya9.us-east-1.rds.amazonaws.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi_qt26'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'F8gqIMeH05fhSW08fJ5jPWjM6ECiL20t'),
-      ssl: env.bool('DATABASE_SSL', true),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'Postgres810'),
+      ssl: {
+        ca: fs.readFileSync(`${__dirname}/certificate/rds-ca-2019-root.pem`).toString(),
+      },
     },
     useNullAsDefault: true,
   },
